cb0faa659accc22b85e936c89af3fc8e360c4a88
Vachana Belgavi <vachanabelgavi@vachanas-mbp.myfiosgateway.com>
2021-09-26 21:35:41 -0400

    Assignment1 person image included

---
+++ Assignment1/src/PersonProfile/Person.java
@@ -0,0 +1,197 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PersonProfile;
+
+import java.util.Date;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author vachanabelgavi
+ */
+public class Person {
+    
+    private String name;
+    private String address;
+    private Date dob;
+    private String phone;
+    private String fax;
+    private String email;
+    private String ssn;
+    private String medical;
+    private String health;
+    private String bankAccount;
+    private String licenseNumber;
+    private String licensePlate;
+    private String VehicleID;
+    //private String serialVehicle;
+    private String DeviceID;
+    private String serialDevice;
+    private String linkedin;
+    private String ip;
+    //private String bioID;
+    private String uniCode;
+    private String home;
+    
+    public ImageIcon icon;
+
+    
+    public String getHome() {
+        return home;
+    }
+
+    public void setHome(String home) {
+        this.home = home;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+    
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedical() {
+        return medical;
+    }
+
+    public void setMedical(String medical) {
+        this.medical = medical;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    public String getBankAccount() {
+        return bankAccount;
+    }
+
+    public void setBankAccount(String bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getVehicleID() {
+        return VehicleID;
+    }
+
+    public void setVehicleID(String VehicleID) {
+        this.VehicleID = VehicleID;
+    }
+
+    public String getDeviceID() {
+        return DeviceID;
+    }
+
+    public void setDeviceID(String DeviceID) {
+        this.DeviceID = DeviceID;
+    }
+
+    public String getSerialDevice() {
+        return serialDevice;
+    }
+
+    public void setSerialDevice(String serialDevice) {
+        this.serialDevice = serialDevice;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+
+    public String getUniCode() {
+        return uniCode;
+    }
+
+    public void setUniCode(String uniCode) {
+        this.uniCode = uniCode;
+    }
+    
+}
